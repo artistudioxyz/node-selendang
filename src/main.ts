e80ca59f183c3e23c1060383e905ca1ce08eeccb
Agung Sundoro <muhagung2001@gmail.com>
2023-01-03 11:32:06 +0700

    feat: add sass and setup scroll container

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,7 @@
-import './style.css'
-import {setupCounter} from './counter'
+import './css/style.scss'
+import readme from "./readme";
+// import {setupCounter} from './counter'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://github.com/artistudioxyz/node-selendang" target="_blank" rel="noreferrer">
-      <img src="/logo.png" class="logo" alt="Selendang Logo" />
-    </a>
-    <h1>~ Node Selendang ~</h1>
-    <p class="read-the-docs">
-      a simple reading process bar library
-    </p>
-    <div class="card">
-		<img src="https://img.shields.io/github/last-commit/artistudioxyz/node-selendang" alt="Last Commit">
-		<img src="https://img.shields.io/github/repo-size/artistudioxyz/node-selendang" alt="Repo Size">
-		<img src="https://img.shields.io/github/package-json/v/artistudioxyz/node-selendang" alt="Version">
-    </div>
-  </div>
-`
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = readme
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
---
+++ src/readme.ts
@@ -0,0 +1,11 @@
+export default `
+	<div id="container-readme">
+		<a href="https://github.com/artistudioxyz/node-selendang" target="_blank" rel="noreferrer">
+		  <img src="/logo.png" class="logo" alt="Selendang Logo" />
+		</a>
+		<h1>~ Node Selendang ~</h1>
+		<div class="read-the-docs">
+			<p>a simple reading process bar library</p>
+		</div>
+	</div>
+`
